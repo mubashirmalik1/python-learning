82df274420ef0127e0268315ede16d6461bff69e
mubashir malik <malikmubashir601@gmail.com>
2023-07-18 11:49:19 -0700

    append linkedlist method

--- LinkList/LinkList.py
+++ LinkList/LinkList.py
@@ -4,11 +4,35 @@ class Node:
       self.next = None
 
 class LinkedList:
-   def __init__(self,value):
-      newNode = Node(value)
-      self.head = newNode
-      self.tail = newNode
-      self.length = 1
+      def __init__(self,value):
+            newNode = Node(value)
+            print(newNode)
+            self.head = newNode
+            self.tail = newNode
+            self.length = 1
+  
+      def printList(self):
+            temp = self.head
+            while temp is not None:
+                  print(temp.value)
+                  temp = temp.next
+
+      def append(self, value):
+            newNode = Node(value)
+           
+            if self.length == 0:
+                self.head = newNode   
+                self.tail = newNode
+            else:
+                 self.tail.next =newNode
+                 self.tail = newNode
+            self.length+=1
+            return True
+      
+
+      
 
 myLinkedList = LinkedList(4)
-print(myLinkedList.head.value)
+myLinkedList.append(55)
+myLinkedList.printList()
+
