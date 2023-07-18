ec2ea24b9e4d66663f58c31117670c1ef33173e9
mubashir malik <malikmubashir601@gmail.com>
2023-07-18 12:24:14 -0700

    pop linkedlist method

--- LinkList/LinkList.py
+++ LinkList/LinkList.py
@@ -6,7 +6,6 @@ class Node:
 class LinkedList:
       def __init__(self,value):
             newNode = Node(value)
-            print(newNode)
             self.head = newNode
             self.tail = newNode
             self.length = 1
@@ -23,15 +22,43 @@ class LinkedList:
             if self.length == 0:
                 self.head = newNode   
                 self.tail = newNode
+                
             else:
                  self.tail.next =newNode
                  self.tail = newNode
             self.length+=1
             return True
       
+      # first we check that linkedlist is empty then return none
+      # then set 2 variables temp and pre 
+      # after that we will run the while loop which will check next is none or not if not while loop will run 
+      # make temp equals to pre and assign temp.next  to temp for next iteration of while loop
+      def pop(self):
+            temp = self.head
+            pre = self.tail
+            if self.length == 0:
+                  return None
+            
+            while(temp.next) :
+                 pre = temp
+                 temp = temp.next
+            
+            self.tail = pre
+            self.tail.next = None
+            self.length -=1
 
+            if(self.length == 0):
+                 self.head = None
+                 self.tail = None
+            
+            return temp
+                 
+           
+
+                  
       
 
 myLinkedList = LinkedList(4)
 myLinkedList.append(55)
+myLinkedList.pop()
 myLinkedList.printList()
